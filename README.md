a4b1482a022b068668523ea852885d8b72458f4b
matthew <saint@matthews-MacBook-Pro.local>
2012-08-17 19:21:17 -0400

    more tests and moved some methods around

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Someone help me make this readme look pretty. Pretty please?
 
 Basic Documentation
 ===================
-# First install the gem and have require 'pbyf' in your app
+First install the gem and have require 'pbyf' in your app
 
 Right now you get access to two classes, PBYF and HistoricalQuote
 
@@ -19,15 +19,26 @@ Similar to above method, but can only do one stock at a time. Interval is like d
 # HistoricalQuote object
 HistoricalQuote is an object that is instantiated with 'HistoricalQuote.new(str_of_stock, from_date, to_date, interval = d)' similar to the method above
 
-Methods
+## Methods
     get_hist_column(column)
 returns an array of a column of the data
-
-    macd(short, long, column)
-returns the Moving Average Convergence Divergence, using the exponential moving average of the data. This is set to using closing data by default if you leave out the column parameter.
 
     stochastic_oscillator
 returns an array of the stocastic oscillator for the data set, beginning 3 days from the from_date the object was initialized with
 
     find_k
 returns an array of %K, which is used to calc the stochastic oscillator. Not sure if its useful for other things so it is a public method
+
+# New Array methods
+Some methods have been added to the Array class as well
+    macd(short, long)
+returns the Moving Average Convergence Divergence, using the exponential moving average of the data. This is set to using closing data by default if you leave out the column parameter.
+
+    exp_moving_average(interval)
+When performed on an array it will return an array with the Exponential Moving Averages for that array, starting with array[interval]. i.e. choosing an interval of 5 will return an array which has 5 fewer elements than the original
+
+    moving_average(interval)
+Like the above but for simple moving averages
+
+    average
+This returns an average of all elements in the array as a float
--- lib/pbyf.rb
+++ lib/pbyf.rb
@@ -31,13 +31,15 @@ end
 # making a class to do calculations using the historical data from yahoo finance
 # maybe refactor so @high @low @close etc. columns are precalculated after initializing. Could be faster/cleaner, I dunno. 
 class HistoricalQuote
-  attr_accessor :data, :close
+  attr_accessor :data, :close, :high, :low
   @@col = {date: 0, open: 1, high: 2, low: 3, close: 4}
 
   def initialize str_of_stock, from_date, to_date, interval = 'd'
     @data = PBYF.get_hist_quote str_of_stock, from_date, to_date, interval = 'd'
     @data.slice!(0)
     @close = get_hist_column(@@col[:close])
+    @high = get_hist_column(@@col[:high])
+    @low = get_hist_column(@@col[:low])
   end
 
   #get a column from the historical quotes CSV table
@@ -58,9 +60,31 @@ class HistoricalQuote
     result
   end
 
-  # calculates MACD using exponential moving averages
+  # this method should return an array of %K, with a value for every row of the data set (every date)
+  def find_k
+    # %K = (Current Close - Lowest Low)/(Highest High - Lowest Low) * 100
+    high = @high
+    low = @low
+    close = @close
+    result = []
+
+    close.each_index do |i|
+      result << ((close[i]-low[i])/(high[i]-low[i]))*100
+    end
+    result
+  end
+
+  # This is the 3 day exponential moving average of %K, which is the convention
+  def stochastic_oscillator interval = 3
+    find_k.exp_moving_average(interval)
+  end
+
+end
+
+class Array
+
   def macd short, long    
-    data = @close
+    data = self
     raise 'Your historical data array is too short!' if (data.size < long)
 
     b = data.exp_moving_average(long)
@@ -77,27 +101,6 @@ class HistoricalQuote
     result
   end
 
-  # this method should return an array of %K, with a value for every row of the data set (every date)
-  def find_k
-    # %K = (Current Close - Lowest Low)/(Highest High - Lowest Low) * 100
-    high = get_hist_column(@@col[:high])
-    low = get_hist_column(@@col[:low])
-    close = get_hist_column(@@col[:close])
-    result = []
-
-    close.each_index do |i|
-      result << ((close[i]-low[i])/(high[i]-low[i]))*100
-    end
-  end
-
-  # This is the 3 day exponential moving average of %K
-  def stochastic_oscillator
-    find_k.exp_moving_average(3)
-  end
-
-end
-
-class Array
   def moving_average interval
     return self if interval == 1
     a = self.dup.map{|i| i.to_f}
